77c3cac5844eb8f8ee1db291a1ba33ca6efb657c
Nick Pegg <nick@nickpegg.com>
2016-12-08 18:24:49 -0800

    Refactor keypad to support arbitray pad shapes
    `nil` is the indicator that we've stepped off the pad, and guarded by
    `fetch_key` and as seen in `actual_pad` (which is the pad on the door in
    this scenario)

--- lib/advent/day2/keypad.rb
+++ lib/advent/day2/keypad.rb
@@ -6,12 +6,9 @@ module Advent
       attr_reader :pad, :presses
       attr_accessor :x, :y
 
-      def initialize(input)
-        @pad = [
-          [1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]
-        ]
+      def initialize(input, pad_type: :example, debug: false)
+        @debug = debug
+        @pad = make_pad(pad_type)
 
         # Initial finger position on the 5
         @x = 1
@@ -35,22 +32,79 @@ module Advent
       end
 
       def move(direction)
+        new_x, new_y = new_spot(direction)
+
+        if fetch_key(new_x, new_y)
+          @x = new_x
+          @y = new_y
+          puts "Moved finger to (#{@x},#{@y}) #{fetch_key(@x, @y)}" if @debug
+        else
+          # Disable this cop since it would cause a readability issue
+          # rubocop:disable Style/IfInsideElse
+          puts "Can't move finger to (#{new_x}, #{new_y}), would be off-pad" if @debug
+          # rubocop:enable Style/IfInsideElse
+        end
+      end
+
+      def new_spot(direction)
+        new_x = @x
+        new_y = @y
+
         case direction
         when 'U'
-          @y -= 1 if @y > 0
+          new_y = @y - 1
         when 'L'
-          @x -= 1 if @x > 0
+          new_x = @x - 1
         when 'R'
-          @x += 1 if @x < 2
+          new_x = @x + 1
         when 'D'
-          @y += 1 if @y < 2
+          new_y = @y + 1
         else
           puts "Invalid finger move direction: #{direction}"
         end
+
+        [new_x, new_y]
+      end
+
+      def fetch_key(x, y)
+        return nil if @pad[y].nil?
+        return nil if y < 0 || y >= @pad.length
+        return nil if x < 0 || x >= @pad[y].length
+
+        @pad[x][y]
       end
 
       def press
         @presses << @pad[y][x]
+      end
+
+      # Returns the pad to use, either the 'example' one given on the keypad
+      # document, or the actual one on the bathroom door
+      def make_pad(mode = :example)
+        case mode
+        when :example
+          example_pad
+        when :actual
+          actual_pad
+        end
+      end
+
+      def example_pad
+        [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]
+        ]
+      end
+
+      def actual_pad
+        [
+          [nil, nil, 1, nil, nil],
+          [nil, 2, 3, 4, nil],
+          [5, 6, 7, 8, 9],
+          [nil, 'A', 'B', 'C', nil],
+          [nil, nil, 'D', nil, nil]
+        ]
       end
     end
   end
