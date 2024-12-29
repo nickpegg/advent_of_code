26c769fad29e10b1acf296fedf6973bf0e5fc14a
Nick Pegg <nick@nickpegg.com>
2024-12-29 10:32:59 -0800

    2024 day10 part2

--- 2024/src/bin/day10.rs
+++ 2024/src/bin/day10.rs
@@ -46,35 +46,54 @@ impl Map {
 fn main() {
     env_logger::init();
     let map = Map::from_str(include_str!("../../data/day10.txt")).unwrap();
-    println!("Part 1: {}", part1(&map));
+    let (score, rating) = walk_map(map);
+
+    println!("Part 1: {}", score);
+    println!("Part 2: {}", rating);
 }
 
-fn part1(map: &Map) -> usize {
+// Do walks from each starting point on the map. Returns the "score" (number of peaks found) and
+// "rating" (number of paths which end up at a peak)
+fn walk_map(map: Map) -> (usize, usize) {
     let mut score = 0;
+    let mut rating = 0;
     for start in map.starts.iter() {
         debug!("Starting walk at {start}");
         let mut peaks = HashSet::new();
-        walk(map, *start, 0, &mut peaks);
+        let walk_rating = walk(&map, *start, 0, &mut peaks);
 
         let walk_score = peaks.len();
         debug!("Got walk score of {walk_score} from {start}");
+        debug!("Got walk rating of {walk_rating} from {start}");
         score += walk_score;
+        rating += walk_rating;
     }
-    score
+
+    (score, rating)
 }
 
 // Try walking from a position in the various directions trying to reach the max height
-fn walk(map: &Map, pos: Position, expected: u8, peaks: &mut HashSet<Position>) {
+// Args:
+//   map: The trail map, which has a grid of elevations
+//   pos: Current position on the map
+//   expected: The expected elevation. If the elevation at the position doesn't match this, then
+//      we're either too high or too low
+//   peaks: A set of known peaks, which is needed to determine the score in part 1
+//
+// Returns: the "rating" score of the start position, which is the number of paths to various peaks
+fn walk(map: &Map, pos: Position, expected: u8, peaks: &mut HashSet<Position>) -> usize {
     debug!("Trying {pos}");
     let val = map.grid[pos.0][pos.1];
     if val != expected {
-        return; // Not moving upward
+        return 0; // Not moving upward
     } else if val == MAX_HEIGHT {
         debug!("  Found a peak @ {pos}");
         peaks.insert(pos);
-        return; // found a peak
+        return 1; // found a peak
     }
     debug!("  Found {val}");
+
+    let mut rating_score = 0;
 
     for direction in DIRECTIONS {
         let Some(new_x) = pos.0.checked_add_signed(direction.0.into()) else {
@@ -88,8 +107,10 @@ fn walk(map: &Map, pos: Position, expected: u8, peaks: &mut HashSet<Position>) {
         }
         let new_pos = Position(new_x, new_y);
 
-        walk(map, new_pos, expected + 1, peaks);
+        rating_score += walk(map, new_pos, expected + 1, peaks);
     }
+
+    rating_score
 }
 
 #[cfg(test)]
@@ -111,9 +132,11 @@ mod day10_tests {
     }
 
     #[test]
-    fn test_part1() {
+    fn test_walk() {
         init();
         let m = Map::from_str(TEST_MAP).unwrap();
-        assert_eq!(part1(&m), 36);
+        let (score, rating) = walk_map(m);
+        assert_eq!(score, 36);
+        assert_eq!(rating, 81);
     }
 }
