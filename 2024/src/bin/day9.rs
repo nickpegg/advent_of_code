38070fd1b5a045f967f450e036662e78ee61eb61
Nick Pegg <nick@nickpegg.com>
2024-12-28 18:12:16 -0800

    2024 day9 part2

--- 2024/src/bin/day9.rs
+++ 2024/src/bin/day9.rs
@@ -5,6 +5,21 @@ use log::{debug, error};
 // A list of block IDs, or None if that block is free space
 struct Disk {
     data: Vec<Option<usize>>,
+    files: Vec<File>,
+}
+
+#[derive(Clone, Debug)]
+struct File {
+    id: usize,
+
+    // Length of the file in blocks
+    len: usize,
+
+    // Index of the start position on disk
+    start: usize,
+
+    // Indicies of all the blocks which contain the file
+    blocks: Vec<usize>,
 }
 
 impl Disk {
@@ -12,6 +27,7 @@ impl Disk {
     // they are free space
     fn from_str(s: &str) -> Result<Disk, ParseIntError> {
         let mut data = Vec::new();
+        let mut files = Vec::new();
 
         let mut cur_file_id = 0;
         let mut is_file = true;
@@ -19,6 +35,7 @@ impl Disk {
             if c.is_whitespace() {
                 continue;
             }
+
             let len = match c.to_string().parse() {
                 Ok(l) => l,
                 Err(e) => {
@@ -26,11 +43,21 @@ impl Disk {
                     return Err(e);
                 }
             };
-            debug!("len: {len}");
+
             if is_file {
+                let mut blocks = Vec::new();
                 for _ in 0..len {
                     data.push(Some(cur_file_id));
+                    blocks.push(data.len() - 1);
                 }
+                let f = File {
+                    id: cur_file_id,
+                    len,
+                    start: blocks[0],
+                    blocks,
+                };
+                files.push(f);
+
                 cur_file_id += 1;
             } else {
                 for _ in 0..len {
@@ -41,7 +68,7 @@ impl Disk {
             is_file = !is_file;
         }
 
-        Ok(Disk { data })
+        Ok(Disk { data, files })
     }
 
     // Returns a string based representation of the data layout, which is easier for visualization
@@ -60,6 +87,10 @@ impl Disk {
     }
 
     // Defragment the data by moving all blocks from the end to the first available free space
+    //
+    // NOTE: Because we don't care about maintaining file block order, this will trash the block
+    // lists in all the files. I don't care to maintain them because it's not needed for this part
+    // of the solution
     fn defrag(&mut self) {
         // Two pointers:
         // - One walks from the front and sits at free space ready to accept a block
@@ -77,9 +108,18 @@ impl Disk {
         }
         while front < rear {
             debug!("f:{front} r:{rear}");
+            let fileno = self.data[rear].unwrap();
+            let file = &mut self.files[fileno];
+
             // Move the data block from the back to the front
             self.data[front] = self.data[rear];
             self.data[rear] = None;
+
+            if file.start == rear {
+                file.start = front;
+                // Trash the file's block list because this defrag screws it up
+                file.blocks.clear();
+            }
 
             // Move the pointers to the next spots
             while let Some(_) = self.data[front] {
@@ -87,6 +127,39 @@ impl Disk {
             }
             while let None = self.data[rear] {
                 rear -= 1;
+            }
+        }
+    }
+
+    // Defragment the data, but keep files contiguous. Again look for free space at the beginning,
+    // but look for files starting starting with the highest ID
+    fn defrag_files(&mut self) {
+        // Go through the files from highest ID to lowest, and through the free spaces from start
+        // to end, and move the file to the earliest free space
+        self.files.sort_by_key(|f| f.id);
+        for mut file in self.files.iter_mut().rev() {
+            let mut start = None;
+            let mut blocks: Vec<usize> = Vec::new();
+
+            // NOTE: Could optimize by scanning once for free blocks and maintaining a list of free
+            // ranges
+            for (i, blk) in self.data.iter().enumerate() {
+                if *blk == None {
+                    if start == None {
+                        start = Some(i);
+                    }
+                    blocks.push(i);
+
+                    // If we found enough space, move the file
+                    if blocks.len() == file.len && start.unwrap() < file.start {
+                        Disk::move_file(&mut self.data, &mut file, blocks);
+                        break;
+                    }
+                } else {
+                    // Found the end of the free space, it didn't fit, so reset and keep looking
+                    start = None;
+                    blocks = Vec::new();
+                }
             }
         }
     }
@@ -103,6 +176,39 @@ impl Disk {
 
         sum
     }
+
+    // Move a file to the specified blocks. The blocks do not need to be contiguous, and do not
+    // need to be in order. The first block index will be the start of the file.
+    fn move_file(
+        disk_blocks: &mut Vec<Option<usize>>,
+        file: &mut File,
+        block_indicies: Vec<usize>,
+    ) {
+        debug_assert_eq!(file.len, block_indicies.len());
+        debug_assert!(block_indicies.len() > 0);
+
+        debug!(
+            "Moving file id:{} from {} to {}",
+            file.id, file.start, block_indicies[0]
+        );
+
+        file.start = block_indicies[0];
+        let old_indicies: Vec<usize> = file.blocks.clone();
+
+        // Move the file's blocks
+        debug!("  File's old blocks: {:?}", file.blocks);
+        debug!("  File's new blocks: {:?}", block_indicies);
+        file.blocks = block_indicies.clone();
+        for idx in block_indicies {
+            disk_blocks[idx] = Some(file.id);
+        }
+
+        // Zero out the old data
+        debug!("  Zeroing out {:?}", old_indicies);
+        for idx in old_indicies {
+            disk_blocks[idx] = None;
+        }
+    }
 }
 
 fn main() {
@@ -110,6 +216,10 @@ fn main() {
     let mut disk = Disk::from_str(include_str!("../../data/day9.txt")).unwrap();
     disk.defrag();
     println!("Part 1: {}", disk.checksum());
+
+    disk = Disk::from_str(include_str!("../../data/day9.txt")).unwrap();
+    disk.defrag_files();
+    println!("Part 2: {}", disk.checksum());
 }
 
 #[cfg(test)]
@@ -124,11 +234,22 @@ mod day9_tests {
 
     // Test expanding a disk map into a block layout
     #[test]
-    fn test_expand() {
+    fn test_from_str() {
         init();
         let d = Disk::from_str(TEST_DISK_MAP).unwrap();
         let expected = "00...111...2...333.44.5555.6666.777.888899";
         assert_eq!(d.as_str(), expected);
+
+        // Make sure all the Files are sane
+        for file in d.files {
+            debug!("Checking file {:#?}", file);
+
+            assert_eq!(file.start, file.blocks[0]);
+            assert_eq!(file.blocks.len(), file.len);
+            for block in file.blocks {
+                assert_eq!(d.data[block], Some(file.id));
+            }
+        }
     }
 
     #[test]
@@ -149,5 +270,15 @@ mod day9_tests {
         let mut d = Disk::from_str(TEST_DISK_MAP).unwrap();
         d.defrag();
         assert_eq!(d.checksum(), 1928);
+    }
+
+    #[test]
+    fn test_file_defrag() {
+        init();
+        let mut d = Disk::from_str(TEST_DISK_MAP).unwrap();
+        d.defrag_files();
+        let expected = "00992111777.44.333....5555.6666.....8888..";
+        assert_eq!(d.as_str(), expected);
+        assert_eq!(d.checksum(), 2858);
     }
 }
